3d706fe6f7028dcf2992c61da8eac2986d288fc6
techtino <techtino74@gmail.com>
2023-08-05 23:48:22 +0100

    Add support for using nix-sops. Note for future self: remember to add secret keys to .sops.yaml when new host built, then re-encrypt secrets

--- common/default.nix
+++ common/default.nix
@@ -143,5 +143,19 @@
       };
     };
   };
+  # This will add secrets.yml to the nix stre
+  # You can avoid this by adding a string to the full path instead, i.e.
+  # sops.defaultSopsFile = "/root/.sops/secrets/example.yaml";
+  sops.defaultSopsFile = ../secrets/example.yaml;
+  # This will automatically import SSH keys as age keys
+  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+  # This is using an age key that is expected to already be in the filesystem
+  sops.age.keyFile = "/var/lib/sops-nix/key.txt";
+  # This will generate a new key if the key specified above does not exist
+  sops.age.generateKey = true;
+  # This is the actual specification of the secrets.
+  sops.secrets.example-key = {};
+  sops.secrets."myservice/my_subdir/my_secret" = {};
+
 
 }
